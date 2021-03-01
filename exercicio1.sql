5ec1ac7ea2ba1f1625daf39a13736fe6861973fe
João Correia <jcjunior.contatos@gmail.com>
2021-02-28 22:53:06 -0300

    Update exercicio1.sql

--- exercicio1.sql
+++ exercicio1.sql
@@ -45,14 +45,14 @@ USE LIVRARIA;
 /* CRIANDO A TABELA DE LIVROS */
 
 CREATE TABLE LIVROS(
-    NOME VARCHAR(30),
+    LIVRO VARCHAR(30),
     AUTOR VARCHAR(30),
     SEXO CHAR(1),
     PAGINAS INT(2),
     EDITORA VARCHAR(20),
     VALOR FLOAT(7,2),
     UF CHAR(2),
-    ANO CHAR(4)
+    ANO INT(4)
 );
 
 
@@ -73,31 +73,107 @@ INSERT INTO LIVROS VALUES('O poder da mente', 'Clara Mafra', 'F', 120, 'Continen
 /* 1 – Trazer todos os dados. */
 
 SELECT * FROM LIVROS;
+'''
++---------------------+-------------------+------+---------+-------------+--------+------+------+
+| LIVRO               | AUTOR             | SEXO | PAGINAS | EDITORA     | VALOR  | UF   | ANO  |
++---------------------+-------------------+------+---------+-------------+--------+------+------+
+| Cavaleiro Real      | Ana Claudia       | F    |     465 | Atlas       |  49.90 | RJ   | 2009 |
+| SQL para leigos     | João Nunes        | M    |     450 | Addison     |  98.00 | SP   | 2018 |
+| Receitas Caseiras   | Celia Tavares     | F    |     210 | Atlas       |  45.00 | RJ   | 2008 |
+| Pessoas Efetivas    | Eduardo Santos    | M    |     390 | Beta        |  78.99 | RJ   | 2018 |
+| Habitos Saudáveis   | Eduardo Santos    | M    |     630 | Beta        | 150.98 | RJ   | 2019 |
+| A Casa Marrom       | Hermes Macedo     | M    |     250 | Bubba       |  60.00 | MG   | 2016 |
+| Estacio Querido     | Geraldo Francisco | M    |     310 | Insignia    | 100.00 | ES   | 2015 |
+| Pra sempre amigas   | Leda Silva        | F    |     510 | Insignia    |  78.98 | ES   | 2011 |
+| Copas Inesqueciveis | Marco Alcantara   | M    |     200 | Larson      | 130.98 | RS   | 2018 |
+| O poder da mente    | Clara Mafra       | F    |     120 | Continental |  56.58 | SP   | 2017 |
++---------------------+-------------------+------+---------+-------------+--------+------+------+
+10 rows in set (0.00 sec)
+'''
 
 /* 2 – Trazer o nome do livro e o nome da editora */
 
-SELECT NOME, EDITORA FROM LIVROS;
+SELECT LIVRO, EDITORA FROM LIVROS;
+'''
++---------------------+-------------+
+| LIVRO               | EDITORA     |
++---------------------+-------------+
+| Cavaleiro Real      | Atlas       |
+| SQL para leigos     | Addison     |
+| Receitas Caseiras   | Atlas       |
+| Pessoas Efetivas    | Beta        |
+| Habitos Saudáveis   | Beta        |
+| A Casa Marrom       | Bubba       |
+| Estacio Querido     | Insignia    |
+| Pra sempre amigas   | Insignia    |
+| Copas Inesqueciveis | Larson      |
+| O poder da mente    | Continental |
++---------------------+-------------+
+10 rows in set (0.00 sec)
+'''
 
 /* 3 – Trazer o nome do livro e a UF dos livros publicados por autores do sexo masculino. */
 
-SELECT NOME, UF FROM LIVROS
+SELECT LIVRO, UF FROM LIVROS
 WHERE SEXO = 'M';
+'''
++---------------------+------+
+| LIVRO               | UF   |
++---------------------+------+
+| SQL para leigos     | SP   |
+| Pessoas Efetivas    | RJ   |
+| Habitos Saudáveis   | RJ   |
+| A Casa Marrom       | MG   |
+| Estacio Querido     | ES   |
+| Copas Inesqueciveis | RS   |
++---------------------+------+
+6 rows in set (0.00 sec)
+'''
 
 /* 4 - Trazer o nome do livro e o número de páginas dos livros publicados por autores do sexo feminino. */
 
-SELECT NOME, PAGINAS FROM LIVROS
+SELECT AUTOR, LIVRO, PAGINAS FROM LIVROS
 WHERE SEXO = 'F';
+'''
++---------------+-------------------+---------+
+| AUTOR         | LIVRO             | PAGINAS |
++---------------+-------------------+---------+
+| Ana Claudia   | Cavaleiro Real    |     465 |
+| Celia Tavares | Receitas Caseiras |     210 |
+| Leda Silva    | Pra sempre amigas |     510 |
+| Clara Mafra   | O poder da mente  |     120 |
++---------------+-------------------+---------+
+4 rows in set (0.00 sec)
+'''
 
 /* 5 – Trazer os valores dos livros das editoras de São Paulo. */
 
-SELECT VALOR FROM LIVROS
+SELECT UF, LIVRO, VALOR FROM LIVROS
 WHERE UF = 'SP';
+'''
++------+------------------+-------+
+| UF   | LIVRO            | VALOR |
++------+------------------+-------+
+| SP   | SQL para leigos  | 98.00 |
+| SP   | O poder da mente | 56.58 |
++------+------------------+-------+
+2 rows in set (0.00 sec)
+'''
 
 /* 6 – Trazer os dados dos autores do sexo masculino que tiveram livros publicados por 
 São Paulo ou Rio de Janeiro (Questão Desafio). */
 
-SELECT NOME, AUTOR, SEXO, PAGINAS, EDITORA, VALOR, UF, ANO
+SELECT LIVRO, AUTOR, SEXO, PAGINAS, EDITORA, VALOR, UF, ANO
 FROM LIVROS
 WHERE (SEXO = 'M' AND UF = 'RJ')
    OR (SEXO = 'M' AND UF = 'SP');
-
+'''
++--------------------+----------------+------+---------+---------+--------+------+------+
+| LIVRO              | AUTOR          | SEXO | PAGINAS | EDITORA | VALOR  | UF   | ANO  |
++--------------------+----------------+------+---------+---------+--------+------+------+
+| SQL para leigos    | João Nunes     | M    |     450 | Addison |  98.00 | SP   | 2018 |
+| Pessoas Efetivas   | Eduardo Santos | M    |     390 | Beta    |  78.99 | RJ   | 2018 |
+| Habitos Saudáveis  | Eduardo Santos | M    |     630 | Beta    | 150.98 | RJ   | 2019 |
++--------------------+----------------+------+---------+---------+--------+------+------+
+3 rows in set (0.00 sec)
+'''
